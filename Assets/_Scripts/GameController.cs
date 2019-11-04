7b2de7f84152936acb07d860b37ddd4bbbe633f9
Andrew Trinidad <atrinid7my.centennialcollege.ca>
2019-11-04 01:17:14 -0500

    Added Scenes and Scene Management

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -15,7 +15,18 @@ public class GameController : MonoBehaviour
     public Text livesLabel;
     public Text scoreLabel;
 
+    public GameObject startButton;
+    public GameObject restartButton;
+
     public GameObject highScore;
+
+    public GameObject grid;
+
+    public GameObject startLabel;
+    public GameObject endLabel;
+    public Text highScoreLabel;
+
+
 
     public int Lives
     {
@@ -27,9 +38,9 @@ public class GameController : MonoBehaviour
         set
         {
             _lives = value;
-            if(_lives < 1)
+            if (_lives < 1)
             {
-
+                SceneManager.LoadScene("End");
             }
 
             else
@@ -50,7 +61,7 @@ public class GameController : MonoBehaviour
         {
             _score = value;
 
-            if(highScore.GetComponent<HighScore>().score < _score)
+            if (highScore.GetComponent<HighScore>().score < _score)
             {
                 highScore.GetComponent<HighScore>().score = _score;
             }
@@ -68,16 +79,75 @@ public class GameController : MonoBehaviour
     void Start()
     {
         highScore = GameObject.Find("HighScore");
+        startButton = GameObject.Find("StartButton");
+        grid = GameObject.Find("Grid");
+        startLabel = GameObject.Find("StartLabel");
+        endLabel = GameObject.Find("EndLabel");
+        restartButton = GameObject.Find("RestartButton");
 
-       Lives = 5;
-       Score = 0;
-
+        DontDestroyOnLoad(highScore);
+        SceneConfiguration();
 
     }
+
+    private void SceneConfiguration()
+    {
+        switch (SceneManager.GetActiveScene().name)
+        {
+            case "Start":
+                scoreLabel.enabled = false;
+                livesLabel.enabled = false;
+                startButton.SetActive(true);
+                grid.SetActive(false);
+                startLabel.SetActive(true);
+                endLabel.SetActive(false);
+                highScoreLabel.enabled = false;
+                restartButton.SetActive(false);
+                break;
+            case "Level1":
+                scoreLabel.enabled = true;
+                livesLabel.enabled = true;
+                startButton.SetActive(false);
+                grid.SetActive(true);
+                startLabel.SetActive(false);
+                endLabel.SetActive(false);
+                highScoreLabel.enabled = false;
+                restartButton.SetActive(false);
+                break;
+            case "End":
+                scoreLabel.enabled = false;
+                livesLabel.enabled = false;
+                startButton.SetActive(false);
+                grid.SetActive(false);
+                startLabel.SetActive(false);
+                endLabel.SetActive(true);
+                highScoreLabel.enabled = true;
+                restartButton.SetActive(true);
+                highScoreLabel.text = "High Score: " + highScore.GetComponent<HighScore>().score;
+                break;
+
+
+        }
+
+        Lives = 5;
+        Score = 0;
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void OnStartButtonClick()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void OnRestartButtonClick()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
 }
